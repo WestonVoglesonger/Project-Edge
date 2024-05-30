17ad0c28e43c33b869a8d3b3827fc59eb69e3962
Weston Voglesonger <111527221+tvogle@users.noreply.github.com>
2024-05-30 13:48:27 -0400

    Implement role-based access control and enhance project form functionality
    - Add current authenticated user as a project owner when creating new
    projects.
    - Restrict project update functionality to project owners.
    - Ensure non-owners can view project details without editing
    capabilities.
    - Implement conditional display of 'Update Project' and 'Cancel' buttons
    based on user ownership.
    - Make form fields, including current users and owners, read-only for
    non-owners and editable for owners during project updates.
    - Fix search functionality for current users and owners with correct
    dropdown display.
    - Improve user experience with double-click to remove functionality for
    owners.

--- frontend/src/app/projects/project-form/project-form.component.ts
+++ frontend/src/app/projects/project-form/project-form.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ProjectService } from '../projects.service';
 import { UserResponse } from 'src/app/shared/users/user.models';
 import { UserService } from 'src/app/shared/users/user.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { minLengthArray } from 'src/app/shared/min-length-array.validator';
 
 @Component({
   selector: 'app-project-form',
@@ -21,11 +23,19 @@ export class ProjectFormComponent implements OnInit {
   isNewProject: boolean = true;
   filteredUsers: UserResponse[] = [];
   filteredOwners: UserResponse[] = [];
+  currentUser: UserResponse | null = null;
+  isOwner: boolean = false;
+
+  @ViewChild('currentUsersInput') currentUsersInput!: ElementRef;
+  @ViewChild('ownersInput') ownersInput!: ElementRef;
+
+  private originalProjectData: any;
 
   constructor(
     private fb: FormBuilder,
     private projectService: ProjectService,
     private userService: UserService,
+    private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router
   ) {
@@ -33,18 +43,34 @@ export class ProjectFormComponent implements OnInit {
       name: ['', [Validators.required, Validators.minLength(3)]],
       description: ['', [Validators.required, Validators.minLength(10)]],
       current_users: this.fb.array([]),
-      owners: this.fb.array([])
+      owners: this.fb.array([], minLengthArray(1))
     });
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = params.get('id');
-      if (id && id !== 'new') {
-        this.isNewProject = false;
-        this.loadProject(id);
+    this.authService.fetchCurrentUser().subscribe(
+      user => {
+        this.currentUser = user;
+        this.route.paramMap.subscribe(params => {
+          const id = params.get('id');
+          if (id && id !== 'new') {
+            this.isNewProject = false;
+            this.loadProject(id);
+          } else {
+            this.addCurrentUserToOwners();
+          }
+        });
+      },
+      error => {
+        console.error('Error fetching current user', error);
       }
-    });
+    );
+  }
+
+  addCurrentUserToOwners(): void {
+    if (this.currentUser) {
+      this.owners.push(this.fb.control(this.currentUser));
+    }
   }
 
   get currentUsers(): FormArray {
@@ -64,6 +90,16 @@ export class ProjectFormComponent implements OnInit {
         });
         this.setCurrentUsers(project.current_users);
         this.setOwners(project.owners);
+        this.originalProjectData = {
+          ...project,
+          current_users: [...project.current_users],
+          owners: [...project.owners]
+        };
+
+        this.isOwner = project.owners.some((owner: { email: string | undefined; }) => owner.email === this.currentUser?.email);
+        if (!this.isOwner) {
+          this.projectForm.disable();
+        }
       },
       error => {
         console.error('Error loading project', error);
@@ -115,13 +151,19 @@ export class ProjectFormComponent implements OnInit {
   searchUsers(event: Event, type: 'users' | 'owners' = 'users'): void {
     const input = event.target as HTMLInputElement;
     const query = input.value;
-
+  
     if (query.length > 2) {
       this.userService.searchUsers(query).subscribe(users => {
+        console.log('Users', users);
+        const selectedUsers = type === 'users' ? this.currentUsers.value : this.owners.value;
+        const selectedUserEmails = selectedUsers.map((user: UserResponse) => user.email);
+  
+        const filtered = users.filter(user => !selectedUserEmails.includes(user.email));
+        
         if (type === 'users') {
-          this.filteredUsers = users;
+          this.filteredUsers = filtered;
         } else {
-          this.filteredOwners = users;
+          this.filteredOwners = filtered;
         }
       },
       error => {
@@ -134,7 +176,7 @@ export class ProjectFormComponent implements OnInit {
         this.filteredOwners = [];
       }
     }
-  }
+  }  
 
   addUser(user: UserResponse, type: 'users' | 'owners', input: HTMLInputElement): void {
     if (type === 'users') {
@@ -143,6 +185,44 @@ export class ProjectFormComponent implements OnInit {
       this.owners.push(this.fb.control(user));
     }
     input.value = ''; // Clear the input field
+  }
+
+  removeUser(index: number, type: 'users' | 'owners'): void {
+    if ((this.isOwner || this.isNewProject) && (type === 'users' || type === 'owners')) {
+      if (type === 'users') {
+        this.currentUsers.removeAt(index);
+      } else {
+        this.owners.removeAt(index);
+      }
+  
+      // Re-fetch users to update the dropdown
+      const inputElement = type === 'users' ? this.currentUsersInput.nativeElement : this.ownersInput.nativeElement;
+      this.searchUsers({ target: inputElement } as Event, type);
+    }
+  }
+
+  cancelChanges(): void {
+    if (this.isNewProject) {
+      this.projectForm.reset();
+      this.currentUsers.clear();
+      this.owners.clear();
+    } else {
+      this.projectForm.patchValue({
+        name: this.originalProjectData.name,
+        description: this.originalProjectData.description,
+      });
+
+      // Clear and reset current_users and owners FormArrays
+      this.currentUsers.clear();
+      this.owners.clear();
+      this.originalProjectData.current_users.forEach((user: UserResponse) => {
+        this.currentUsers.push(this.fb.control(user));
+      });
+      this.originalProjectData.owners.forEach((user: UserResponse) => {
+        this.owners.push(this.fb.control(user));
+      });
+    }
+    this.router.navigate(['/projects']);
   }
 
   get f(): { [key: string]: AbstractControl } {
